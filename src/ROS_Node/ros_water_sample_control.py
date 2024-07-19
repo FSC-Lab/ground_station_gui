11961b0c4602db6fe6a3535abf8bca059314f5a5
tommyzhng <tommyz2005@gmail.com>
2024-07-19 17:36:42 -0400

    feat: Adjust stepper motor control

--- src/ROS_Node/ros_water_sample_control.py
+++ src/ROS_Node/ros_water_sample_control.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QMessageBox
 import Common
 
 from geometry_msgs.msg import Vector3Stamped
+from std_msgs.msg import Float32MultiArray
 
 class WaterSampleRosNode(QObject):
     update_data = pyqtSignal(int)
@@ -18,8 +19,8 @@ class WaterSampleRosNode(QObject):
         #self.payload_pos_vel_sub = rospy.Subscriber("/encoder/speed_payload", Vector3Stamped, self.payload_pos_spd_callback)
 
         # define publishers
-        self.cmd_length_pub = rospy.Publisher("/encoder/setpoint_length", Vector3Stamped, queue_size=10)
-
+        self.cmd_stepper_pub = rospy.Publisher("/stepper/setpoint", Float32MultiArray, queue_size=1)
+        self.cable_test_pub = rospy.Publisher("/stepper/test", Float32MultiArray, queue_size=1)
 
         self.rate = rospy.Rate(5)
 
@@ -34,12 +35,18 @@ class WaterSampleRosNode(QObject):
     def payload_pos_callback(self, msg):
         self.data_struct.update_payload_pos(msg.vector.x, msg.vector.y, msg.vector.z)
 
-    def pub_payload_length(self, len):
-        length_msg = Vector3Stamped()
-        length_msg.header.stamp = rospy.Time.now()
-        length_msg.vector.z = len
-        self.cmd_length_pub.publish(length_msg)
+    def pub_stepper_cmd(self, len, spd):
+        cmd_msg = Float32MultiArray()
+        cmd_msg.data = [len, spd]
+        self.cmd_stepper_pub.publish(cmd_msg)
 
+    def pub_stepper_cmd(self, amplitude, speed, startCMD):
+        test_msg = Float32MultiArray()
+        if startCMD:
+            test_msg.data = [1, amplitude, speed]
+            self.cable_test_pub.publish(test_msg)
+            return
+        test_msg.data = [0, amplitude, speed]
         
     def run(self):
         while not rospy.is_shutdown():
@@ -72,9 +79,12 @@ class WaterSampleRosThread():
         self.ros_object.connect_update_gui(self.update_gui_data)
 
         # gui2ros
-        self.ui.btnSendLength.clicked.connect(self.send_length_request)
+        self.ui.btnSendCMD.clicked.connect(self.send_cable_cmd_req)
         self.ui.btnUp.clicked.connect(self.send_up_incr_request)
         self.ui.btnDown.clicked.connect(self.send_down_incr_request)
+        self.ui.btnStartTest.clicked.connect(lambda: self.send_test_request(True))
+        self.ui.btnStopTest.clicked.connect(lambda: self.send_test_request(False))
+
 
     def update_gui_data(self, data):
         if not self.lock.tryLock():
@@ -93,22 +103,23 @@ class WaterSampleRosThread():
         self.ui.PayloadY_DISP.display(int(self.payload_pos_msg.y))
         self.ui.PayloadZ_DISP.display(int(self.payload_pos_msg.z))
 
-    def display_current_length(self):
-        self.ui.length_CMD.setText(str(self.payload_pos_msg.z))
-
-    def send_length_request(self):
-        self.ros_object.pub_payload_length(float(self.ui.length_CMD.text()))
+    def send_cable_cmd_req(self):
+        self.ros_object.pub_stepper_cmd(float(self.ui.length_CMD.text()), float(self.ui.speed_CMD.text()))
 
     def send_down_incr_request(self):
         self.send_stop_request()
         abs_length = self.payload_pos_msg.z + float(self.ui.increment_CMD.text())
-        self.ros_object.pub_payload_length(abs_length)
+        self.ros_object.pub_stepper_cmd(abs_length, 0.5) # 0.5 m/s
 
     def send_up_incr_request(self):
         self.send_stop_request()
         abs_length = self.payload_pos_msg.z - float(self.ui.increment_CMD.text())
-        self.ros_object.pub_payload_length(abs_length)
+        self.ros_object.pub_stepper_cmd(abs_length, 0.5)
 
     def send_stop_request(self):
-        self.ros_object.pub_payload_length(float(self.ui.length_CMD.text()))
+        self.ros_object.pub_stepper_cmd(float(self.ui.length_CMD.text()), 0)
 
+    ## testing
+    def send_test_request(self, startCMD=True):
+        self.send_stop_request()
+        self.ros_object.pub_stepper_cmd(float(self.ui.test_amp_CMD.text()), float(self.ui.test_speed_CMD.text()), startCMD)
